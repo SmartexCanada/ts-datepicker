a2153cd35780e86abe1509e06e3a9375845a6de0
smartexcan <95705436+smartexcan@users.noreply.github.com>
2025-07-15 13:56:58 -0400

    Export additional types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 export { DatePicker as default } from "./datepicker";
+export * from "./types/datepicker";
+export * from "./types/locale";
+export * from "./types/options";
+export type {
+    ChangeDateEventDetail,
+    ChangeViewDateEventDetail,
+    ChangeViewEventDetail,
+    FocusEventDetail,
+} from "./types/events";
